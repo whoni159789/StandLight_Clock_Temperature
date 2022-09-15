3451692e1d6a1471a6862dc2a4ce7acbf59dd3a9
whoni159789 <whoni6131@gmail.com>
2022-09-15 16:37:24 +0900

    Temp, Humid Display

---
+++ src/app/Service/Service.cpp
@@ -0,0 +1,87 @@
+#include "Service.h"
+
+Service::Service(View *viewer)
+{
+    lightState = LIGHT_OFF;
+    view = viewer;
+}
+
+Service::~Service()
+{
+
+}
+
+void Service::updateState(std::string strState)
+{
+    switch (lightState)
+    {
+        case LIGHT_OFF:
+            if (strState == "modeButton") 
+            {
+                lightState = LIGHT_1;
+                view->setState(lightState);
+            }
+            
+        break;
+
+        case LIGHT_1:
+            if (strState == "modeButton") 
+            {
+                lightState = LIGHT_2;
+            }
+            if (strState == "powerButton") 
+            {
+                lightState = LIGHT_OFF;
+            }
+            view->setState(lightState);
+        break;
+
+        case LIGHT_2:
+            if (strState == "modeButton") 
+            {
+                lightState = LIGHT_3;
+            }
+            if (strState == "powerButton") 
+            {
+                lightState = LIGHT_OFF;
+            }
+            view->setState(lightState);
+        break;
+
+        case LIGHT_3:
+            if (strState == "modeButton") 
+            {
+                lightState = LIGHT_4;
+            }
+            if (strState == "powerButton") 
+            {
+                lightState = LIGHT_OFF;
+            }
+            view->setState(lightState);
+        break;
+
+        case LIGHT_4:
+            if (strState == "modeButton") 
+            {
+                lightState = LIGHT_5;
+            }
+            if (strState == "powerButton") 
+            {
+                lightState = LIGHT_OFF;
+            }
+            view->setState(lightState);
+        break;
+
+        case LIGHT_5:
+            if (strState == "modeButton") 
+            {
+                lightState = LIGHT_OFF;
+            }
+            if (strState == "powerButton") 
+            {
+                lightState = LIGHT_OFF;
+            }
+            view->setState(lightState);
+        break;
+    }
+}
